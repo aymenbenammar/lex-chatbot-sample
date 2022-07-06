7e797da37d515f2c475cf3dbadaed05183460ac1
aymen ben ammar <aymen.benammar@etudiant-isi.utm.tn>
2022-07-06 15:02:58 +0100

    added chat popup

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React,  {useState}  from 'react';
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
 import '@fontsource/inter/variable.css';
+import { FcOnlineSupport } from "react-icons/fc";
 import { Amplify , Auth } from 'aws-amplify';
 import { AmplifyChatbot } from '@aws-amplify/ui-react/legacy';
+import img from './chatbot.jpg'
 
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 
 Amplify.configure({
   Auth: {
-    identityPoolId: 'xxxx',
+    identityPoolId: 'us-east-1:c647c422-16b9-41ed-b16f-ba3f18b46c9c',
     region: 'us-east-1'
   },
   Interactions: {
@@ -23,20 +25,38 @@ Amplify.configure({
     }
   }
 });
-
+let  hide = {
+  display :'none'
+}
+let show = {
+  display: 'block'
+}
 function App() {
-
+const [chatopen,setChatopen] = useState(false); 
+const toggle = () =>{
+  setChatopen(!chatopen)
+}
 
   return (
     <div className="App">
-   
-        <AmplifyChatbot
+      <div id='chatCon' >
+        <div className='chat-box' style={chatopen? show : hide}>
+          <div className=''>    
+          <AmplifyChatbot
     botName="BookTrip"
-    botTitle="aaaa"
+    botTitle="Booktrip Bot"
     welcomeMessage="Hello, how can I help you?"
     voiceEnabled={true}	
-  />
-            </div>
+  /></div>
+        </div>
+        <div className='pop'> 
+      
+  <p onClick={toggle}><img onClick={toggle}  src={img} alt="click her"/></p>
+                                   
+        </div>
+      </div>
+    
+    </div>
 
   );
 }
